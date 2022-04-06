eb6ca81b68dfc636d334689ea4f87fb521fe7352
KostiaevSV <skprod.info@gmail.com>
2022-04-06 11:27:30 +0400

    HW done

---
+++ src/js/classCharacter.js
@@ -0,0 +1,36 @@
+export default class Character {
+    constructor(name, type, attack, defence) {
+      const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+  
+      if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+        throw new Error('Некорректное значение имени');
+      }
+  
+      if (typeof type !== 'string' || !types.includes(type)) {
+        throw new Error('Некорректное значение типа');
+      }
+  
+      this.name = name;
+      this.type = type;
+      this.health = 100;
+      this.level = 1;
+      this.attack = attack;
+      this.defence = defence;
+    }
+  
+    levelUp() {
+      if (this.health <= 0) {
+        throw new Error('Нельзя повысить level умершего');
+      }
+      this.level += 1;
+      this.attack *= 1.2;
+      this.defence *= 1.2;
+      this.health = 100;
+    }
+  
+    damage(points) {
+      if (this.health >= 0) {
+        this.health -= points * (1 - this.defence / 100);
+      }
+    }
+}
